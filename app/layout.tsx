cc4af6531a48aa37af55cc646b6e57a3cdd994b1
jeffigy <evangelistajeffy25@gmail.com>
2023-05-26 21:42:37 +0800

    second  build passed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,6 @@ import DashboardLayout from "./components/Layout/layout";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { theme } from "./chakra/theme";
-export const metadata = {
-  title: "Reusable Dashboard",
-  description: "Reusable Dashboard",
-};
-
 export default function RootLayout({
   children,
 }: {
